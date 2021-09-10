c3e2c4401f06c927c098c5c58c12fb95c50f18fd
mariia <rmashka2001@gmail.com>
2021-09-10 12:54:45 +0300

    21

--- lessons/lesson21/task1/index.js
+++ lessons/lesson21/task1/index.js
@@ -1,7 +1,16 @@
 export function getTitleElement() {
     //
+    const titleElem = document.querySelector('.title');
+
+    return console.dir(titleElem);
 }
 
 export function getInputElement() {
     //
+    const inputElem = document.querySelector('[type="text"]');
+    console.dir(inputElem);
+    return inputElem;
+
 }
+getTitleElement();
+getInputElement();
