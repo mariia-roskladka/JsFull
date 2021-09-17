b13b554560f2acdf300fea31915040dd43d1e5e1
mariia <rmashka2001@gmail.com>
2021-09-17 20:28:28 +0300

    28.1

--- lessons/lesson28/task1/index.js
+++ lessons/lesson28/task1/index.js
@@ -1,5 +1,5 @@
-const calc = initValue => {
-    let result = initValue;
+const calc = (initialValue) => {
+    let result = initialValue;
     const calculator = {
         add(value) {
             result += value;
@@ -9,19 +9,26 @@ const calc = initValue => {
             result *= value;
             return calculator;
         },
-        div(value) {
-            result /= value;
-            return calculator;
-        },
         subtract(value) {
             result -= value;
             return calculator;
         },
+        substract(value) {
+            result -= value;
+            return calculator;
+        },
+        div(value) {
+            result /= value;
+            return calculator;
+        },
         result() {
             return result;
-        }
+        },
     };
+
     return calculator;
 };
-const result = calc(3).add(2).mult(4).div(10).subtract(5).result(); // -3
+
+const result = calc(3).add(2).mult(4).div(10).substract(5).result();
+
 console.log(result);
