552ec5ab6d2c701a96d970ee161ed478c268f1be
mariia <rmashka2001@gmail.com>
2021-09-18 17:26:09 +0300

    29

---
+++ lessons/lesson29/task1/index.js
@@ -0,0 +1,27 @@
+const addImage = (imgSrc, callback) => {
+    const imgElem = document.createElement("img");
+    imgElem.setAttribute("alt", "My photo");
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector(".page");
+    containerElem.append(imgElem);
+
+    imgElem.addEventListener("load", () => callback(null, imgElem));
+    imgElem.addEventListener("error", (e) => callback("Image load is failed"));
+};
+
+const onImageLoaded = (error, data) => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    const sizeElem = document.querySelector(".image-size");
+
+    sizeElem.textContent = `${data.width} x ${data.height}`;
+};
+
+// examples;
+addImage(
+    // "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg",
+    "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigsstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg",
+    onImageLoaded
+);
