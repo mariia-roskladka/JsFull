ea9671f628f2ccd15d257a497a1c729e71541bab
mariia <rmashka2001@gmail.com>
2021-09-24 16:02:08 +0300

    33.2

--- lessons/lesson33/task2/index.js
+++ lessons/lesson33/task2/index.js
@@ -1,18 +1,11 @@
-const baseUrl = "https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks";
+const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
-function getTasksList() {
-    return fetch(baseUrl).then((response) => response.json());
+export function getTasksList() {
+    // getTasksList logic
+    return fetch(baseUrl).then(response => response.json());
 }
 
-function getTaskById(taskId) {
-    return fetch(`${baseUrl}/${taskId}`).then((response) => response.json());
+export function getTaskById(taskId) {
+    //  getTaskById logic
+    return fetch(`${baseUrl}/${taskId}`).then(response => response.json());
 }
-
-// examples
-getTasksList().then((tasksList) => {
-    console.log(tasksList); // array of the task objects - [ {'id':'1', 'done':false ... }, {'id':'2', 'done':true ... }, ...]
-});
-
-getTaskById("2").then((taskData) => {
-    console.log(taskData); // {'id':'2', 'done':true ... }
-});
