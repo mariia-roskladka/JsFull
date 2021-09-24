667f249b569c970303aebec4b32a55b4352976a0
mariia <rmashka2001@gmail.com>
2021-09-24 15:58:50 +0300

    33.1

--- lessons/lesson33/task1/index.js
+++ lessons/lesson33/task1/index.js
@@ -1,32 +1,32 @@
-const userNameElem = document.querySelector('.user__name');
-const userLocationElem = document.querySelector('.user__location');
+const userAvatarElem = document.querySelector(".user__avatar");
+const usernameElem = document.querySelector(".user__name");
+const userLocationELem = document.querySelector(".user__location");
 
-const defaultAvatar = 'https://avatar3.githubusercontent.com/u10001';
+const defaultAvatar = "https://avatars3.githubusercontent.com/u10001";
 
 userAvatarElem.src = defaultAvatar;
 
-const fetchUserData = userName => {
-    return fetch(`https://api.github.com/users/{$userName}`)
-        .then(response => response.json());
+// https;//api.github.com/users/USERNAME
+
+// fetch and render user data ...
+
+const showUserBtnElem = document.querySelector(".name-form__btn");
+const userNameInput = document.querySelector(".name-form__input");
+
+const fetchUserData = (userName) =>
+    fetch(`https://api.github.com/users/${userName}`) // if we need only GET we may not put second parameter for fetch usually it looks like fetch(url,options)
+    .then((response) => response.json());
+
+const renderUserData = (userData) => {
+    const { avatar_url, name, location } = userData;
+    userAvatarElem.src = avatar_url;
+    usernameElem.textContent = name;
+    userLocationELem.textContent = location ? `from ${location}` : "";
 };
-
-
-const renderUserData = userData => {
-    console.log(userData);
-    const {} = userData;
-};
-
-
-
-
-const showUserBtnElem = document.querySelector('.name-form__btn');
-const userNameInputElem = document.querySelector('.name-form__info');
-
 
 const onSearchUser = () => {
-    const userName = userNameInputElem.value;
-    fetchUserData(userName)
-        .then(userData => renderUserData(userData));
+    const userName = userNameInput.value;
+    fetchUserData(userName).then((userData) => renderUserData(userData));
 };
 
-showUserBtnElem.addEventListener('click', onSearchUser);
+showUserBtnElem.addEventListener("click", onSearchUser);
