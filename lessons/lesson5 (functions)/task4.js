4f529846e1f5e2fd37163f9da668fe277de68a93
mariia <rmashka2001@gmail.com>
2021-12-11 18:59:31 +0200

    /

---
+++ lessons/lesson5 (functions)/task4.js
@@ -0,0 +1,10 @@
+//lesson 4 Стрелочная ф-ция возводящая число в квадрат
+
+// function square(number) {
+//     return number * number
+// }
+
+
+
+const square = (num) => num * num;
+console.log(square(8));
--- lessons/lesson5/task4.js
+++
@@ -1,5 +0,0 @@
-//lesson 4 Стрелочная ф-ция возводящая число в квадрат
-
-function square(number) {
-    return number * number
-}
