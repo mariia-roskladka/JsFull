a7794f23b2c3a64f9bbfe97e7067c6a3f0a2694a
mariia <rmashka2001@gmail.com>
2021-12-11 19:14:06 +0200

    7

--- lessons/lesson7 (array methods)/task1.js
+++ lessons/lesson7 (array methods)/task1.js
@@ -2,12 +2,10 @@
 function getSpecialNumbers(array) {
 
     let resultArray = array.filter(function(k) {
-            if (k % 3 === 0) {
-                return k
-            }
+        if (k % 3 === 0) {
+            return k
         }
-
-    )
+    })
     return resultArray;
 }
 console.log(getSpecialNumbers([1, 2, 3, 4, 5, 1, 2]))
